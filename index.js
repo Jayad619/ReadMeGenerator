ee00c940a5e3f24440677ae112351dd48898a29a
Jayad Arshad <jayadusama@hotmail.co.uk>
2022-08-18 01:31:55 +0100

    installed dependencies and started index.js

---
+++ index.js
@@ -0,0 +1,39 @@
+// require modules 
+const fs = require('fs'); 
+const inquirer = require('inquirer'); 
+
+// linking to page where the README is developed 
+const generatePage = require('./utils/generateMarkdown.js');
+
+// array of questions for user
+const questions = () => {
+
+    // using inquirer to prompt questions to user 
+    return inquirer.prompt([
+    {
+        type: 'input',
+        name: 'github',
+        message: 'What is your Github Username? Enter Below.',
+        validate: nameInput => {
+            if (nameInput) {
+                return true;
+            } else {
+                console.log('Enter your github username!');
+                return false; 
+            }
+        } 
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'Input your email address please',
+        validate: nameInput => {
+            if (nameInput) {
+                return true;
+            } else {
+                console.log('Email Address please!');
+                return false; 
+            }
+        }
+
+    },  
